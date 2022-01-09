e7f5e9af92aace7c1e316a2668ef8a9a143b5b35
Victor Meudic <victormeudic@gamil.com>
2022-01-09 12:13:49 +0100

    FormEvents

--- index.js
+++ index.js
@@ -81,3 +81,32 @@ window.addEventListener("scroll", () => {
     nav.style.top = "-50px";
   }
 });
+
+//------------------------------------------------------------
+//Forms event
+const inputName = document.querySelector('input[type="text"]');
+const select = document.querySelector("select");
+const form = document.querySelector("form");
+let pseudo = "";
+let language = "";
+
+inputName.addEventListener("input", (e) => {
+  pseudo = e.target.value;
+});
+
+select.addEventListener("input", (e) => {
+  language = e.target.value;
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (cgv.checked) {
+    document.querySelector("form > div").innerHTML = `
+    <h3>Pseudo : ${pseudo}</h3>
+    <h4>Langage préféré : ${language}</h4>
+    `;
+  } else {
+    alert("Veuillez acceptez les CGV");
+  }
+});
