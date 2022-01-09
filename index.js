82676525262fe4114a72e4d84515dd518d5d4baf
Victor Meudic <victormeudic@gamil.com>
2022-01-09 11:38:29 +0100

    ScrollEvent

--- index.js
+++ index.js
@@ -66,3 +66,18 @@ document.addEventListener("keypress", (e) => {
     keypresssContainer.style.background = "pink";
   }
 });
+
+//-------------------------------------------------------
+// Scroll Event
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+
+  if (window.scrollY > 120) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = "-50px";
+  }
+});
