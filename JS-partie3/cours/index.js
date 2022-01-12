65918397713871f659bb209dcb390f952877aa2c
Victor Meudic <victormeudic@gamil.com>
2022-01-12 18:32:16 +0100

    progression

---
+++ JS-partie3/cours/index.js
@@ -0,0 +1,83 @@
+// Rappel des types des donées
+let string = "Chaine";
+let number = 25;
+let boolean = true;
+let maVariable; // type undefined
+
+// Tableaux
+let array = ["Bordeaux", "Toulouse", "Nantes"];
+// console.log(array[2]);
+
+let array2 = ["Bordeaux", 24, true, [1, 2], { nom: "Denis" }];
+//console.log(array2[4].nom);
+let objet = {
+  pseudo: "Denis",
+  age: 33,
+  technos: ["Javascript", "React", "NodeJs"],
+  admin: false,
+};
+
+// objet.adresse = "son adresse";
+
+// console.log(objet);
+
+let data = [
+  {
+    pseudo: "Denis",
+    age: 33,
+    technos: ["Javascript", "React", "NodeJs"],
+    admin: false,
+  },
+  {
+    pseudo: "Samia",
+    age: 24,
+    technos: ["CSS", "React", "NodeJs"],
+    admin: false,
+  },
+  {
+    pseudo: "Nicolas",
+    age: 42,
+    technos: ["PHP", "React", "NodeJs"],
+    admin: true,
+  },
+];
+
+//console.log(data[2].pseudo);
+
+//--------------------------------------
+// Les structures de controle
+if (data[0].age > data[1].age) {
+  //console.log(data[0].pseudo + " est plus agé que " + data[1].pseudo);
+} else {
+}
+
+// While
+let w = 0;
+
+while (w < 10) {
+  w++;
+  //console.log(w);
+}
+
+// Do while
+let d = 0;
+
+do {
+  d++;
+  //console.log(d);
+} while (d < 5);
+
+// Les boucles for
+for (const user of data) {
+  //document.body.innerHTML += `<li>${user.pseudo} - ${user.age} ans</li>`;
+}
+
+for (i = 0; i < data.length; i++) {
+  //console.log(i);
+  //console.log(data[i].technos[0]);
+  //document.body.innerHTML += "<h2>" + data[i].pseudo + "</h2>";
+}
+
+document.body.addEventListener("click", (e) => {
+  console.log(e.target.id);
+});
