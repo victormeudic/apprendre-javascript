66c92b3a3ba0125ea42ddc04316e447f51fe8b10
Victor Meudic <victormeudic@gamil.com>
2022-01-16 16:21:31 +0100

    progression du cours

--- JS-partie4/cours/index.js
+++ JS-partie4/cours/index.js
@@ -23,3 +23,37 @@ fetch("data.txt").then((res) => res.text());
 
 fetch("data.json").then((res) => res.json());
 //.then((data) => console.log(data));
+
+const myHeaders = new Headers();
+
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+};
+
+//fetch("data.json", init).then((res) => console.log(res));
+
+// CRUD => Create (Post), read (Get), update (Put), Delete
+
+// Asynchrone
+
+setTimeout(() => {
+  console.log("test");
+}, 2000);
+
+// Promise
+//fetch("myLink").then((res) => res);
+
+// asyn/await
+async function fetchData() {
+  await fetch("monLien");
+  // attend que le await soit exécuté avant de faire la suite
+  executFonction();
+}
+
+const fetchData2 = async () => {
+  await fetch("monLien");
+  // attend que le await soit exécuté avant de faire la suite
+  executFonction();
+};
