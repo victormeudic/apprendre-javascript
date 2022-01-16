90900f224abbcab3b0591833eb3686127fb40da4
Victor Meudic <victormeudic@gamil.com>
2022-01-16 11:47:41 +0100

    générateur de blague

---
+++ JS-partie4/cours/index.js
@@ -0,0 +1,25 @@
+// XMLHttpRequest
+
+function reqListener() {
+  //console.log(this.responseText);
+}
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+//req.open("get", "data.txt", true);
+// req.open("get", "data.json", true);
+req.open("get", "https://api.blablagues.net/?rub=blagues", true);
+req.send();
+
+// FETCH
+
+// fetch("monlien", "objet d'options")
+//   .then((response) => {
+//     // response
+//   })
+//   .catch((error) => console.log(error));
+
+fetch("data.txt").then((res) => res.text());
+//.then((data) => console.log(data));
+
+fetch("data.json").then((res) => res.json());
+//.then((data) => console.log(data));
